436970c35ed49e871a08cdf1c55647f84ee490dc
Abhishek Datta <abhishek_datta@intuit.com>
2026-02-08 22:29:48 -0800

    Fix OAuth path

--- netlify/functions/auth.js
+++ netlify/functions/auth.js
@@ -74,11 +74,14 @@ exports.handler = async (event, context) => {
     }
 
     // Return access token
+    // Decap CMS expects the token in the response body
     return {
       statusCode: 200,
       headers: {
         'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type'
       },
       body: JSON.stringify({ token: tokenData.access_token })
     };
